5ccea6546ba4b4aebed3120d441f5086cd479b73
Victor Presumido <victor.presumido@gmail.com>
2016-08-31 04:00:13 -0300

    Working on play method

--- board.rb
+++ board.rb
@@ -5,11 +5,21 @@ class Board
     self.width = minesweeper.instance_variable_get("@width")
     num_mines = minesweeper.instance_variable_get("@num_mines")
     @body = Array.new(width) { Array.new(height,'.') }
+    @game = minesweeper
     plant_mines(num_mines)
   end
 
+  # Getters
   def body
     self.instance_variable_get("@body")
+  end
+
+  def game
+    self.instance_variable_get("@game")
+  end
+
+  def set_body x,y,val
+    @body[x-1][y-1]=val
   end
 
   # Plant mines into the board
@@ -24,9 +34,60 @@ class Board
   end
 
   def board_state(*args)
-    if :xray
-      # todo
+    board_state = {:unknown_cell =>[],:clear_cell=>[], :bomb =>[], :flag =>[]}
+    for i in 0..self.width-1
+      for j in 0..self.height-1
+        case self.body[i][j]
+        when "."
+          board_state[:unknown_cell] << [i+1,j+1]
+        when "F"
+          board_state[:flag] << [i+1,j+1]
+        else
+          board_state[:clear_cell] << [i+1,j+1]
+          # TODO add number of nerighbor bombs
+        end
+      end
     end
+    if :xray && !self.game.still_playing?
+      for i in 0..self.width-1
+        for j in 0..self.height-1
+          if self.body[i][j] == "#"
+            board_state[:bomb] << [i+1,j+1]
+          end
+        end
+      end
+    end
+    puts board_state
+  end
+
+  def has_bomb? x,y
+    print_board
+    if self.body[x-1][y-1]=="#"
+      puts "bomba em #{x},#{y}"
+      true
+    else
+      false
+    end
+  end
+
+  def show_neighbors x,y
+    puts "foi pra #{x},#{y}"
+    if !self.has_bomb?(x,y) && self.valid_bounds?(x,y)
+      self.body[x-1][y-1] = "L"
+      show_neighbors(x-1,y-1) # Upper left
+      show_neighbors(x,y-1) # Upper
+      show_neighbors(x+1,y-1) # Upper right
+      show_neighbors(x-1,y) # Left
+      show_neighbors(x+1,y) # Right
+      show_neighbors(x-1,y+1) # Bottom left
+      show_neighbors(x,y+1) # Bottom
+      show_neighbors(x+1,y+1) # Botom Right
+    end
+
+  end
+
+  def valid_bounds? x,y
+    x <= self.width && y <= self.height && x>0 && y>0
   end
 
   def print_board
--- minesweeper.rb
+++ minesweeper.rb
@@ -15,29 +15,51 @@ class Minesweeper
     self.instance_variable_get("@board")
   end
 
-  def play
+  def still_playing?
+    false
+  end
 
+  def play x,y
+    if (valid_play?(x,y) && self.board.valid_bounds?(x,y))
+      if self.board.has_bomb? x,y
+        puts "Você perdeu! As minas eram:"
+        puts self.board.board_state({xray: true}) # PrettyPrinter.new.print(game.board_state(xray: true))
+      else
+        self.board.show_neighbors x,y
+      end
+
+      true
+    else
+      false
+    end
   end
 
   def flag x,y
-    if x <= self.board.width && y <= self.board.height
+    if self.board.valid_bounds? x,y
       cell = self.board.body[x-1][y-1]
       if cell!="F"
         self.board.body[x-1][y-1]="F"
       elsif cell=="F"
         self.board.body[x-1][y-1]="."
+      else
+
       end
       true
     end
     false
   end
+
+  def valid_play? x,y
+    cell = self.board.body[x-1][y-1]
+    cell!="F" && cell!=" "
+  end
 end
 
-game = Minesweeper.new(2,2,1)
+game = Minesweeper.new(3,3,2)
 game.board.print_board
-game.flag 2,2
-game.board.print_board
+# game.flag 2,2
+# game.board.print_board
 puts "------------"
-game.flag 2,3
+game.play 2,3
 game.board.print_board
-game.board.board_state({xray: true})
+# game.board.board_state({xray: true})
